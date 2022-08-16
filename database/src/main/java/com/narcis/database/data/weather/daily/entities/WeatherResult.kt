1adda2e013ce40cf6e665f5fed6b5652ecea905b
Nargesdeypir <nargesdeypir@gmail.com>
2022-08-16 20:08:31 +0430

    leaving office

--- database/src/main/java/com/narcis/database/data/weather/daily/entities/WeatherResult.kt
+++ database/src/main/java/com/narcis/database/data/weather/daily/entities/WeatherResult.kt
@@ -1,4 +1,26 @@
 package com.narcis.database.data.weather.daily.entities
 
-class WeatherResult {
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+
+
+@Entity(
+    tableName = WeatherEntity.WeatherSchema.TABLE_NAME,
+
+)
+data class WeatherEntity(
+
+    @ColumnInfo(name = WeatherSchema.ID) val id : Int
+) {
+    object WeatherSchema {
+        const val TABLE_NAME =  "weather"
+        const val ID = "id"
+        const val MAIN = "main"
+        const val DESCRIPTION = "description"
+        const val TEMP = "temp"
+        const val TEMP_MIN = "tempMin"
+        const val TEMP_MAX = "tempMax"
+        const val NAME = "name"
+
+    }
 }
