24760d1e04c7d50807db1d9605d8e033088908a2
narges deypir <nargesdeypir@gmail.com>
2022-08-30 22:39:35 +0430

    more test

--- app/src/test/java/com/narcis/openweatherinterview/ui/viewModel/WeatherViewModelTest.kt
+++ app/src/test/java/com/narcis/openweatherinterview/ui/viewModel/WeatherViewModelTest.kt
@@ -1,9 +1,31 @@
 package com.narcis.openweatherinterview.ui.viewModel
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.github.javafaker.Faker
+import com.narcis.model.weatherActions.LocationModel
+import com.narcis.openweatherinterview.coroutine.MainTestCoroutine
+import com.narcis.openweatherinterview.domain.useCase.GetCurrentLocationUseCase
+import io.mockk.impl.annotations.MockK
 import org.junit.Rule
+
 
 const val currentVersionDate = "20220826"
 class WeatherViewModelTest {
+    @get:Rule
+    val mainTestCoroutine = MainTestCoroutine()
+
+    @get:Rule
+    val instantTaskCoroutine = InstantTaskExecutorRule()
+
+    @MockK
+    private lateinit var getCurrentLocationUseCase: GetCurrentLocationUseCase
+
+    private val locationModel by lazy {
+        LocationModel(
+            Faker().address().latitude().toDouble(),
+            Faker().address().longitude().toDouble()
+        )
+    }
 
 
 
